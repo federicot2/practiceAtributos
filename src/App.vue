3044f4f0a8a5d4bc4e48e11580858b76ed587f7c
federicot2 <87830686+federicot2@users.noreply.github.com>
2023-04-03 12:03:07 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col-md-4" v-for="input in inputs" :key="input.model">
+    <label>
+      {{ input.model }}:
+      <input type="text" v-model="identificadoresSalida[input.model]" class="form-control" />
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputs: [
+        {
+          type: 'text',
+          model: 'cedula'
+        },
+        {
+          type: 'text',
+          model: 'nombre'
+        },
+        {
+          type: 'text',
+          model: 'apellido'
+        }
+      ],
+      identificadoresSalida: {
+        cedula: '',
+        nombre: '',
+        apellido: ''
+      }
+    }
+  }
+}
+</script>
