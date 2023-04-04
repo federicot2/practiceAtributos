371ee9557a34f00f3804d9c9d5829039c65ca01b
federicot2 <87830686+federicot2@users.noreply.github.com>
2023-04-04 01:55:02 -0500

    ultima actualizacion

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,69 @@
 <template>
-  <div class="col-md-4" v-for="input in inputs" :key="input.model">
-    <label>
-      {{ input.model }}:
-      <input type="text" v-model="identificadoresSalida[input.model]" class="form-control" />
-    </label>
+  <!--SELECT DE MOVILIDAD-->
+  <div class="row">
+    <div class="col-md-4">
+      <label>
+        Seleccione Movilidad:
+        <select v-model="opcion" class="form-select">
+          <option value="">Seleccione</option>
+          <option value="1">Entrada</option>
+          <option value="0">Salida</option>
+        </select>
+      </label>
+    </div>
   </div>
-</template>
+  <!--DIV CON INPUTS-->
+  <div class="row">
+    <div class="col-md-4" v-for="input in inputs" :key="input.model">
+      <label>
+        {{ input.label }}
+        <input :type="input.type" :value="datos[0][input.model]" class="form-control" />
+      </label>
+    </div>
+  </div>
 
+
+  <!--BOTON-->
+  <div class="row">
+    <div class="col-md-12">
+      <br>
+      <button type="submit" class="btn btn-primary">Enviar</button>
+    </div>
+  </div>
+
+</template>
 <script>
+
 export default {
   data() {
     return {
+      opcion: '',
       inputs: [
         {
           type: 'text',
-          model: 'cedula'
+          model: 'cedula',
+          label: 'Cédula:'
         },
         {
           type: 'text',
-          model: 'nombre'
+          model: 'nombre',
+          label: 'Nombre:'
         },
         {
           type: 'text',
-          model: 'apellido'
+          model: 'apellido',
+          label: 'Apellido:'
         }
       ],
-      identificadoresSalida: {
-        cedula: '',
-        nombre: '',
-        apellido: ''
-      }
+      datos: [
+        {
+          cedula: '',
+          nombre: '',
+          apellido: ''
+        }
+      ]
     }
   }
 }
+
 </script>
