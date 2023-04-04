403cd2c0d2f57071aa8ef4e1b00b7f316f7bf822
federicot2 <87830686+federicot2@users.noreply.github.com>
2023-04-04 10:01:50 -0500

    select

--- src/App.vue
+++ src/App.vue
@@ -15,25 +15,37 @@
   <!--DIV CON INPUTS-->
   <div class="row">
     <div class="col-md-4" v-for="input in inputs" :key="input.model">
-      <label>
-        {{ input.label }}
-        <input :type="input.type" :value="datos[0][input.model]" class="form-control" />
-      </label>
+      <div v-if="input.filtro.includes(parseInt(opcion))">
+        <label>
+          {{ input.label }}
+          <input :type="input.type" v-model="datos[0][input.model]" class="form-control" />
+        </label>
+      </div>
     </div>
   </div>
-
+  
+  <!--SELECT-->
+  <div class="row">
+    <div class="col-md-4" v-for="select in selects" :key="select.model">
+      <div v-if="select.filtro.includes(parseInt(opcion))">
+        <label>{{select.label}}</label>
+        <select class="form-select" v-model="datos[0][select.model]">
+          <option v-for="option in select.options" :value="option.id">{{option.texto}}</option>
+        </select>
+      </div>
+    </div>
+  </div>
 
   <!--BOTON-->
   <div class="row">
     <div class="col-md-12">
-      <br>
+      <br />
       <button type="submit" class="btn btn-primary">Enviar</button>
     </div>
   </div>
-
 </template>
-<script>
 
+<script>
 export default {
   data() {
     return {
@@ -42,28 +54,61 @@ export default {
         {
           type: 'text',
           model: 'cedula',
-          label: 'Cédula:'
+          label: 'Cédula:',
+          filtro: [1]
         },
         {
           type: 'text',
           model: 'nombre',
-          label: 'Nombre:'
+          label: 'Nombre:',
+          filtro: [0]
+        },
+        {
+          type: 'text',
+          model: 'pasaporte',
+          label: 'Pasaporte:',
+          filtro: [0]
         },
         {
           type: 'text',
           model: 'apellido',
-          label: 'Apellido:'
+          label: 'Apellido:',
+          filtro: [1]
+        },
+        {
+          type: 'text',
+          model: 'telefono',
+          label: 'Telefono:',
+          filtro: [3]
+        }
+      ],
+      selects:[
+        {
+          model: 'programa',
+          label: 'Programa:',
+          filtro:[1],
+          options:[
+            {
+              id:3,
+              texto:"Erasmus"
+            },
+            {
+              id:4,
+              texto:"DITIC"
+            }
+          ],
         }
       ],
       datos: [
         {
           cedula: '',
           nombre: '',
-          apellido: ''
+          apellido: '',
+          pasaporte:'',
+          programa:''
         }
       ]
     }
   }
 }
-
 </script>
