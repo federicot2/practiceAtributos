d71fe710cb701fac2a0bf2c42da53aac753a93c3
federicot2 <87830686+federicot2@users.noreply.github.com>
2023-04-10 03:26:36 -0500

    cambios

--- src/App.vue
+++ src/App.vue
@@ -4,34 +4,56 @@
     <div class="col-md-4">
       <label>
         Seleccione Movilidad:
-        <select v-model="opcion" class="form-select">
+        <select v-model="datos[0].movilidad" class="form-select">
           <option value="">Seleccione</option>
-          <option value="1">Entrada</option>
-          <option value="0">Salida</option>
+          <option value="entrada">Entrada</option>
+          <option value="salida">Salida</option>
         </select>
       </label>
     </div>
   </div>
+
+
+
+
   <!--DIV CON INPUTS-->
-  <div class="row">
+  <!--<div class="row">
     <div class="col-md-4" v-for="input in inputs" :key="input.model">
-      <div v-if="input.filtro.includes(parseInt(opcion))">
+
+      <div v-if="input.filtro.includes(datos[0].programa)">
+
         <label>
           {{ input.label }}
           <input :type="input.type" v-model="datos[0][input.model]" class="form-control" />
         </label>
       </div>
     </div>
-  </div>
+  </div>-->
+
+
+
 
   <!--SELECT-->
   <div class="row">
     <div class="col-md-4" v-for="select in selects" :key="select.model">
-      <div v-if="select.filtro.includes(parseInt(opcion))">
+      <div v-if="select.filtro.includes(datos[0].movilidad)">
         <label>{{ select.label }}</label>
         <select class="form-select" v-model="datos[0][select.model]">
           <option v-for="option in select.options" :value="option.id" :key="option.id">{{ option.texto }}</option>
         </select>
+      </div>
+    </div>
+  </div>
+
+
+  <!--INPUTS-->
+  <div class="row">
+    <div class="col-md-4" v-for="input in getInputs()" :key="input.model">
+      <div>
+        <label>
+          {{ input.label }}
+          <input :type="input.type" v-model="datos[0][input.model]" class="form-control" />
+        </label>
       </div>
     </div>
   </div>
@@ -44,64 +66,87 @@
       <button type="submit" class="btn btn-primary">Enviar</button>
     </div>
   </div>
+  <div>
+    <p>{{ datos }}</p>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      opcion: '',
       inputs: [
         {
           type: 'text',
           model: 'cedula',
           label: 'Cédula:',
-          filtro: [1]
+          filtro:'1'
         },
         {
           type: 'text',
           model: 'nombre',
           label: 'Nombre:',
-          filtro: [0]
+          filtro:'2'
         },
         {
           type: 'text',
           model: 'pasaporte',
           label: 'Pasaporte:',
-          filtro: [0]
+          filtro:'3'
         },
         {
           type: 'text',
           model: 'apellido',
           label: 'Apellido:',
-          filtro: [1]
+          filtro:'4'
         },
         {
           type: 'text',
           model: 'telefono',
           label: 'Telefono:',
-          filtro: [3]
+          filtro:'5'
         }
       ],
       selects: [
         {
           model: 'programa',
-          label: 'Programa:',
-          filtro: [1],
+          label: 'Programa Entrada:',
+          filtro: ["entrada"],
           options: [
             {
-              id: 3,
-              texto: "Erasmus"
+              id: 'erasmus',
+              texto: "Erasmus Entrada",
+              inputs: ['2', '4']
             },
             {
-              id: 4,
-              texto: "DITIC"
+              id: 'ditic',
+              texto: "DITIC Entrada",
+              inputs: ['1', '3', '5']
+            }
+          ],
+        },
+        {
+          model: 'programa',
+          label: 'Programa Salida:',
+          filtro: ["salida"],
+          options: [
+            {
+              id: 'proSalida1',
+              texto: "programaSalida",
+              inputs: ['1', '3', '5']
+            },
+            {
+              id: 'proSalida2',
+              texto: "programaSalida",
+              inputs: ['2', '4']
             }
           ],
         }
+
       ],
       datos: [
         {
+          movilidad: '',
           cedula: '',
           nombre: '',
           apellido: '',
@@ -110,6 +155,18 @@ export default {
         }
       ]
     }
+  },
+  methods: {
+    
+    getInputs() {
+      const programa = this.datos[0].programa;
+
+      const select = this.selects.find(s => s.model === 'programa');
+      const inputs = select.options.find(o => o.id === programa)?.inputs || [];
+      return this.inputs.filter(input => inputs.includes(input.filtro));
+      
+    }
   }
+
 }
 </script>
