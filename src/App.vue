d34e645766dcdf4a80b284bd738dc982fef5e966
federicot2 <87830686+federicot2@users.noreply.github.com>
2023-04-11 11:36:15 -0500

    update11abril

--- src/App.vue
+++ src/App.vue
@@ -27,17 +27,6 @@
   </div>
 
 
-  <!--INPUTS
-  <div class="row">
-    <div class="col-md-4" v-for="input in getInputs()" :key="input.model">
-      <div>
-        <label>
-          {{ input.label }}
-          <input :type="input.type" v-model="datos[0][input.model]" class="form-control" />
-        </label>
-      </div>
-    </div>
-  </div>-->
 <!--INPUTS-->
 <div class="row">
   <div class="col-md-4" v-for="input in getInputs()" :key="input.model">
@@ -49,16 +38,12 @@
       <label v-else>
         {{ input.label }}
         <select v-model="datos[0][input.model]" class="form-control">
-          <option v-for="option in input.options" :value="option" :key="option">{{ option }}</option>
+          <option v-for="option in input.options" :value="option.id" :key="option.id">{{ option.text }}</option>
         </select>
       </label>
     </div>
   </div>
 </div>
-
-
-  
-
 
   <!--BOTON-->
   <div class="row">
@@ -75,48 +60,172 @@ export default {
     return {
       inputs: [
         {
+          type: 'date',
+          model: 'fechaNacimiento',
+          label: 'Fecha de nacimiento:',
+          filtro: '0'
+
+        },
+        {
+          type: 'text',
+          model: 'nacionalidad',
+          label: 'Nacionalidad:',
+          filtro: '1'
+
+        },
+        {
           type: 'text',
           model: 'nombre',
           label: 'Nombre:',
-          filtro: '1'
+          filtro: '2'
         },
         {
           type: 'text',
           model: 'apellido',
           label: 'Apellido:',
-          filtro: '2'
+          filtro: '3'
         },
         {
           type: 'text',
           model: 'id',
           label: 'id/cedula:',
-          filtro: '3'
-        },
-        {
-          type: 'text',
-          model: 'origen',
-          label: 'origen:',
           filtro: '4'
         },
         {
-          type: 'date',
-          model: 'date',
-          label: 'fecha:',
+          type: 'text',
+          model: 'pasaporte',
+          label: 'pasaporte:',
           filtro: '5'
         },
         {
           type: 'file',
-          model: 'file',
-          label: 'pdf:',
-          filtro: '6'
+          model: 'pasaportePdf',
+          label: 'PDF DE IMG. PASAPORTE:',
+          filtro: '5.1'
         },
         {
           type: 'select',
-          model: 'color',
-          label: 'Color Favorito:',
-          filtro:'7',
-          options: ['rojo', 'azul', 'verde']
+          model: 'origen',
+          label: 'Origen:',
+          filtro: '6',
+          options:['Panamá','Rep.Chiriqui']
+        },
+        {
+          type: 'select',
+          model: 'destino',
+          label: 'Destino:',
+          filtro: '7',
+          options:['Panamá','Rep.Chiriqui']
+        },
+
+        {
+          type: 'select',
+          model: 'continente',
+          label: 'Continente:',
+          filtro: '8',
+          options:['America','Asia','Africa','Oceania','Europa']
+        },
+        {
+          type: 'select',
+          model: 'nivelAcademico',
+          label: 'Nivel Academico',
+          filtro: '9',
+          options: ['1', '2', '3','4','5']
+        },
+        {
+          type: 'select',
+          model: 'añoAcademico',
+          label: 'Año Academico:',
+          filtro:'10',
+          options: ['1', '2', '3','4','5']
+        },
+        {
+          type: 'select',
+          model: 'modalidad',
+          label: 'Modalidad:',
+          filtro:'11',
+          options: [{
+            id: 1,
+            text:"Presencial"
+          },
+          {
+            id:2,
+            text:"Virtual"
+          }
+        ]
+        },
+        {
+          type: 'select',
+          model: 'programa1',
+          label: 'Programa:',
+          filtro:'12',
+          options: ['1', '2']
+        },
+        {
+          type: 'select',
+          model: 'semestreUtp',
+          label: 'Semestre UTP:',
+          filtro:'13',
+          options: ['1', '2']
+        },
+        {
+          type: 'select',
+          model: 'sedeUtp',
+          label: 'Sede UTP:',
+          filtro:'14',
+          options: ['Central','Cocle']
+        },
+        {
+          type: 'select',
+          model: 'facultad',
+          label: 'Facultad UTP:',
+          filtro:'15',
+          options: ['Sistema', 'Industrial', 'Civil','etc']
+        },
+        {
+          type: 'select',
+          model: 'carreraUtp',
+          label: 'Carrera UTP:',
+          filtro:'16',
+          options: ['Lic.Desarrollo de software', '2', '3','4','5']
+        },
+        {
+          type: 'date',
+          model: 'fInicio',
+          label: 'Fecha de Inicio:',
+          filtro:'17'
+        },
+        {
+          type: 'date',
+          model: 'fFinal',
+          label: 'Fecha Final:',
+          filtro:'18'
+        },
+        {
+          type: 'text',
+          model: 'cPersonal',
+          label: 'Contacto Personal:',
+          filtro:'19'
+        },
+        {
+          type: 'text',
+          model: 'cEmergencia',
+          label: 'Contacto de Emergencia:',
+          filtro:'20'
+        },
+        {
+          type: 'text',
+          model: 'cDri',
+          label: 'Contacto de Dri:',
+          filtro:'21'
+        },
+        {
+          type: 'text',
+          model: 'cInternacional',
+          label: 'Contacto Internacional:',
+          filtro:'110'
         }
+
 
       ],
       selects: [
@@ -128,22 +237,22 @@ export default {
             {
               id: 'iaste',
               texto: "IASTE Entrada",
-              inputs: ['2', '4', '6','7']
+              inputs: ['0','1', '2', '3','4','5','5.1','6','7','8','9','11','12','16','17','18','20','21','22','110']
             },
             {
               id: 'sAcademico',
               texto: "Semestre Academico Entrada",
-              inputs: ['1', '3', '5']
+              inputs: ['0','1', '2', '3','4','5','5.1','6','7','8','9','11','12','16','17','18','20','21','110']
             },
             {
               id: 'investigacion',
               texto: "Investigación Entrada",
-              inputs: ['1', '3', '5']
+              inputs: ['1', '2', '3','4','5','5.1','6','7','8','9','11','12','14','17','18','19','20','110']
             },
             {
               id: 'estanciaCorta',
               texto: "Estancias Cortas Entrada",
-              inputs: ['1', '3', '5']
+              inputs: ['1', '2', '3','4','5','5.1','6','7','8','11','12','14','15','17','18','19','20','21','110']
             }
           ],
         },
@@ -155,22 +264,22 @@ export default {
             {
               id: 'iaste',
               texto: "IASTE Salida",
-              inputs: ['1', '3', '5']
+              inputs: ['0','1', '2', '3','4','5','5.1','6','7','8','9','11','12','16','17','18','20','21','22']
             },
             {
               id: 'sAcademico',
               texto: "Semestre Academico Salida",
-              inputs: ['2', '4', '6']
+              inputs: ['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21']
             },
             {
               id: 'investigacion',
               texto: "Investigación Salida",
-              inputs: ['1', '3', '5']
+              inputs: ['1', '2', '3','4','5','5.1','6','7','8','9','11','12','14','17','18','19','20','21']
             },
             {
               id: 'estanciaCorta',
               texto: "Estancias Cortas Salida",
-              inputs: ['1', '3', '5']
+              inputs: ['1', '2', '3','4','5','5.1','6','7','8','11','12','14','15','17','18','19','20','21','110']
             }
           ],
         }
